ea7bb1870323709a1c439f1f88b0d6fb8d0c2d1c
KamonohashiPerry <economics.teru@gmail.com>
2020-09-22 18:17:21 +0900

    update

---
+++ Lesson_5/Sample4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int res;
+
+    cout << "整数を入力してください。\n";
+    cin >> res;
+
+    if (res == 1){ // 関係演算子
+        cout << "1が入力されました。\n";
+    }
+    else if(res == 2){
+        cout << "2が入力されました。\n";
+    }
+    else{
+        cout << "1か2を入力してください。\n";
+    }
+    return 0;
+}
